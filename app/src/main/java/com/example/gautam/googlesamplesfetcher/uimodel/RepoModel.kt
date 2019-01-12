51c0bbe5ce3ae5bcfff45b05251cf091be696b31
Gautam Hans <gautamhans1@gmail.com>
2019-01-12 20:25:09 +0530

    RepoModel class added

--- app/src/main/java/com/example/gautam/googlesamplesfetcher/uimodel/RepoModel.kt
+++ app/src/main/java/com/example/gautam/googlesamplesfetcher/uimodel/RepoModel.kt
@@ -1,2 +1,15 @@
 package com.example.gautam.googlesamplesfetcher.uimodel
 
+import android.os.Handler
+
+class RepoModel {
+
+    fun refreshData(onDataReadyCallback: OnDataReadyCallback) {
+        Handler().postDelayed({onDataReadyCallback.onDataReady("new data")}, 2000)
+    }
+
+    interface OnDataReadyCallback {
+        fun onDataReady (data : String)
+    }
+
+}
