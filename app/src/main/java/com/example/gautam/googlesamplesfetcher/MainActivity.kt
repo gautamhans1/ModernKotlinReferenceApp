be27cc7177ecd63046857e696ffaa4a47692af70
Gautam Hans <gautamhans1@gmail.com>
2019-01-12 21:15:48 +0530

    Added Lifecycle Aware Component to save instance
    -- Extended MainViewModel with ViewModel()
    -- removed local declaration of viewModel instance
    -- ViewModelProvider is responsible to make new instance if it is called first time or to return old instance once when your Activity/Fragment is recreated.

--- app/src/main/java/com/example/gautam/googlesamplesfetcher/MainActivity.kt
+++ app/src/main/java/com/example/gautam/googlesamplesfetcher/MainActivity.kt
@@ -1,24 +1,21 @@
 package com.example.gautam.googlesamplesfetcher
 
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.example.gautam.googlesamplesfetcher.databinding.ActivityMainBinding
-import com.example.gautam.googlesamplesfetcher.uimodel.Repository
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-
-    var mainViewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.viewModel = mainViewModel
+        binding.viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         binding.executePendingBindings()
     }
 }
--- app/src/main/java/com/example/gautam/googlesamplesfetcher/MainViewModel.kt
+++ app/src/main/java/com/example/gautam/googlesamplesfetcher/MainViewModel.kt
@@ -1,10 +1,11 @@
 package com.example.gautam.googlesamplesfetcher
 
+import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 import com.example.gautam.googlesamplesfetcher.data.RepoModel
 import com.example.gautam.googlesamplesfetcher.data.RepoModel.OnDataReadyCallback
 
-class MainViewModel {
+class MainViewModel : ViewModel(){
     var repoModel: RepoModel = RepoModel()
     val text = ObservableField("old data")
     var isLoading = ObservableField(false)
