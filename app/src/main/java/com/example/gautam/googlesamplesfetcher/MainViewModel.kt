eed5c56a5c70d2a9cf777acb2d588d2cbe949bc4
Gautam Hans <gautamhans1@gmail.com>
2019-01-12 20:41:40 +0530

    Using anonymous class declaration for object

--- app/src/main/java/com/example/gautam/googlesamplesfetcher/MainViewModel.kt
+++ app/src/main/java/com/example/gautam/googlesamplesfetcher/MainViewModel.kt
@@ -1,20 +1,21 @@
 package com.example.gautam.googlesamplesfetcher
 
-import com.example.gautam.googlesamplesfetcher.data.RepoModel;
-import com.example.gautam.googlesamplesfetcher.data.RepoModel.OnDataReadyCallback;
+import android.databinding.ObservableField
+import com.example.gautam.googlesamplesfetcher.data.RepoModel
+import com.example.gautam.googlesamplesfetcher.data.RepoModel.OnDataReadyCallback
 
 class MainViewModel {
-    var repoModel : RepoModel = RepoModel()
-    var text : String = ""
-    var isLoading : Boolean = false
+    var repoModel: RepoModel = RepoModel()
+    val text = ObservableField<String>()
+    var isLoading = ObservableField<Boolean>()
 
-    val onDataReadyCallback = object : OnDataReadyCallback {
-        override fun onDataReady(data: String) {
-            text = data
-        }
-    }
-
-    fun refresh () {
-        repoModel.refreshData(onDataReadyCallback)
+    fun refresh() {
+        isLoading.set(true)
+        repoModel.refreshData(object : OnDataReadyCallback {
+            override fun onDataReady(data: String) {
+                isLoading.set(false)
+                text.set(data)
+            }
+        })
     }
 }
