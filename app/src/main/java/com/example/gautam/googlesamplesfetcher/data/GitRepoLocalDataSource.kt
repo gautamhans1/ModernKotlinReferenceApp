d8af20c181d1b744a1318e093cdaedc2b36693f2
Gautam <gautamhans1@gmail.com>
2019-01-27 16:51:35 +0530

    saving the data in local source

--- app/src/main/java/com/example/gautam/googlesamplesfetcher/data/GitRepoLocalDataSource.kt
+++ app/src/main/java/com/example/gautam/googlesamplesfetcher/data/GitRepoLocalDataSource.kt
@@ -1,7 +1,9 @@
 package com.example.gautam.googlesamplesfetcher.data
 
 import com.example.gautam.googlesamplesfetcher.uimodel.Repository
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Single
 import java.util.concurrent.TimeUnit
 
 class GitRepoLocalDataSource {
@@ -14,8 +16,12 @@ class GitRepoLocalDataSource {
         return Observable.just(arrayList).delay(2, TimeUnit.SECONDS)
     }
 
-    fun saveRepositories(arrayList: ArrayList<Repository>) {
-        // TODO(1) save repositories in database
+    fun saveRepositories(arrayList: ArrayList<Repository>): Completable {
+
+        /* Single.just(1) will create Single that emits just number 1.
+         And adding delay of 1 second will delay the emitting by one second */
+
+        return Single.just(1).delay(1, TimeUnit.SECONDS).toCompletable();
     }
 
 }
--- app/src/main/java/com/example/gautam/googlesamplesfetcher/data/GitRepoRepository.kt
+++ app/src/main/java/com/example/gautam/googlesamplesfetcher/data/GitRepoRepository.kt
@@ -14,8 +14,19 @@ class GitRepoRepository(val netManager: NetManager) {
 
         netManager.isConnectedToInternet?.let {
             if (it) {
-                // todo (2) save remote data to local db
-                remoteDataSource.getRepositories()
+
+                /* Using .flatMap ,once remoteDataSource.getRepositories() emits item,
+                that item will be mapped to new Observable that emits same item.
+                That new Observable we created from Completable that saves the same
+                emitted item in the local data store converting it to Single that emits
+                the same emitted item. Cause we need to return Observable, we have to convert
+                that Single to Observable. */
+
+                return remoteDataSource.getRepositories().flatMap {
+                    return@flatMap localDataSource.saveRepositories(it)
+                        .toSingleDefault(it)
+                        .toObservable()
+                }
             }
         }
         return localDataSource.getRepositories()
