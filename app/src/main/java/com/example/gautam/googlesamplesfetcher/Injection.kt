d3f8875ae33acbb7c3b80503977713ae6b9013f5
Gautam <gautamhans1@gmail.com>
2019-01-27 17:32:16 +0530

    Optimized Injection object

--- app/src/main/java/com/example/gautam/googlesamplesfetcher/Injection.kt
+++ app/src/main/java/com/example/gautam/googlesamplesfetcher/Injection.kt
@@ -1,2 +1,22 @@
 package com.example.gautam.googlesamplesfetcher
 
+import android.content.Context
+import com.example.gautam.googlesamplesfetcher.androidmanagers.NetManager
+import com.example.gautam.googlesamplesfetcher.data.GitRepoRepository
+
+object Injection {
+
+    private fun provideNetManager(context: Context): NetManager {
+        return NetManager(context)
+    }
+
+    private fun gitRepoRepository(netManager: NetManager): GitRepoRepository {
+        return GitRepoRepository(netManager)
+    }
+
+    fun provideMainViewModelFactory(context: Context): MainViewModelFactory {
+        val netManager = provideNetManager(context)
+        val repository = gitRepoRepository(netManager)
+        return MainViewModelFactory(repository)
+    }
+}
--- app/src/main/java/com/example/gautam/googlesamplesfetcher/MainActivity.kt
+++ app/src/main/java/com/example/gautam/googlesamplesfetcher/MainActivity.kt
@@ -7,24 +7,22 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.example.gautam.googlesamplesfetcher.adapters.RepositoryRecyclerViewAdapter
-import com.example.gautam.googlesamplesfetcher.androidmanagers.NetManager
-import com.example.gautam.googlesamplesfetcher.data.GitRepoRepository
 import com.example.gautam.googlesamplesfetcher.databinding.ActivityMainBinding
 import com.example.gautam.googlesamplesfetcher.uimodel.Repository
 
 class MainActivity : AppCompatActivity(), RepositoryRecyclerViewAdapter.OnItemClickListener {
     lateinit var binding: ActivityMainBinding
     private val repositoryRecyclerViewAdapter = RepositoryRecyclerViewAdapter(arrayListOf(), this)
+    private lateinit var mainViewModelFactory: MainViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val repository = GitRepoRepository(NetManager(applicationContext))
-        val mainViewModelFactory = MainViewModelFactory(repository)
-        val viewModel = ViewModelProviders.of(this, mainViewModelFactory).
-            get(MainViewModel::class.java)
+        mainViewModelFactory = Injection.provideMainViewModelFactory(applicationContext)
+        val viewModel = ViewModelProviders.of(this, mainViewModelFactory)
+            .get(MainViewModel::class.java)
 
         binding.viewModel = viewModel
         binding.executePendingBindings()
